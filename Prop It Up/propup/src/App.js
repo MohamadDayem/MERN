d3791a8fb004fda06a01024feaa3939dd6d54508
mohamaddayem <u@example.com>
2023-08-15 16:34:35 +0300

    n

--- Prop It Up/propup/src/App.js
+++ Prop It Up/propup/src/App.js
@@ -2,12 +2,14 @@ import MyNewComponent from './components/MyNewComponent';
 import './App.css';
 
 function App() {
+
+// 
   return (
     <div className="App">
       <header className="App-header">
-        <MyNewComponent firstName = {"Kareem"} lastName={"Taha"} age = {25} hairColor={"bold"} />
+        <MyNewComponent firstName = {"Kareem"} lastName={"Taha"} age = {76} hairColor={"bold"} />
         <MyNewComponent firstName = {"enzo"} lastName={"dayem"} age = {25} hairColor={"bold"} />
-        <MyNewComponent firstName = {"muath"} lastName={"Taha"} age = {25} hairColor={"bold"} />
+    
       </header>
     </div>
   );
--- Prop It Up/propup/src/components/MyNewComponent.js
+++ Prop It Up/propup/src/components/MyNewComponent.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
     
 const MyNewComponent = (props) => {
+    const [age, setAge] = useState(props.age)
+    const handleClick = () => {
+        setAge(age+1);
+    }
+
     return (
         <div>
             <h1>
-                My name is {props.firstName} {props.lastName} and my age is {props.age}Y/O and i'm {props.hairColor}
+                My name is {props.firstName} {props.lastName} and my age is {age}Y/O and i'm {props.hairColor}
+                <button onClick={ handleClick }>Click Me</button>
+                
             </h1>
         </div>
     );
